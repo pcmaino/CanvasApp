62c6261a3a1f2513efd2737412c5e42190de8108
Paul <pcmaino@gmail.com>
2019-09-11 10:36:41 -0400

    Started formatting XML files, using ConstraintView

--- app/src/main/java/com/example/canvasapp/MainActivity.java
+++ app/src/main/java/com/example/canvasapp/MainActivity.java
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    //goal: Place two balls on the screen and draw a line between them
+
+    //notes for later:
+    //SplashScreen with fade in/out
+    //Toasts
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
