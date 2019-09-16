b47f5ed6c54e97631e191455db8c7bc35b67eb5d
Paul <pcmaino@gmail.com>
2019-09-16 00:18:42 -0400

    Added onClick methods for buttons, planned out functionality for next time.

--- app/src/main/java/com/example/canvasapp/MainActivity.java
+++ app/src/main/java/com/example/canvasapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.canvasapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
     //goal: Place two balls on the screen and draw a line between them
@@ -15,5 +18,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button graphButton = findViewById(R.id.graphButton);
+        graphButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        }
+        );
+
+        ToggleButton redButton = findViewById(R.id.redButton);
+        redButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //when this is clicked, unclick blue, and vice versa
+            }
+        }
+        );
+
+        ToggleButton blueButton = findViewById(R.id.blueButton);
+        blueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        }
+        );
     }
 }
